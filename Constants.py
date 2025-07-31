9c6a98523e443b198135810f92fb1bd75abf456a
williambrown <140029632+brogment@users.noreply.github.com>
2025-07-31 16:21:28 -0500

    feat: create splash screen, add images and sound placeholders
    Create runnable splash screen with buttons borrowed from 'Dodger',
    add other placeholder images and sounds to appropriate folders

---
+++ Constants.py
@@ -0,0 +1,8 @@
+SCENE_SPLASH = 'scene splash'
+SCENE_PLAY = 'scene play'
+SCENE_HIGH_SCORES = 'scene high scores'
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+BLACK = (0,0,0)
---
+++ SceneSplash.py
@@ -0,0 +1,43 @@
+import pygwidgets
+import pyghelpers
+from Constants import *
+
+class SceneSplash(pyghelpers.Scene):
+    def __init__(self, window):
+        self.window = window
+        self.startButton = pygwidgets.CustomButton(self.window, (250,500),
+                                                   up='images/startNormal.png',
+                                                   down='images/startDown.png',
+                                                   over='images/startOver.png',
+                                                   disabled='images/startDisabled.png',
+                                                   enterToActivate=True)
+
+        self.quitButton = pygwidgets.CustomButton(self.window, (30, 500),
+                                                  up='images/quitNormal.png',
+                                                  down='images/quitDown.png',
+                                                  over='images/quitOver.png',
+                                                  disabled='images/quitDisabled.png')
+
+        self.highScoresButton = pygwidgets.CustomButton(self.window, (360, 500),
+                                                        up='images/gotoHighScoresNormal.png',
+                                                        down='images/gotoHighScoresDown.png',
+                                                        over='images/gotoHighScoresOver.png',
+                                                        disabled='images/gotoHighScoresDisabled.png')
+
+    def getSceneKey(self):
+        return SCENE_SPLASH
+
+    def handleInputs(self, events, keyPressedList):
+        for event in events:
+            if self.startButton.handleEvent(event):
+                self.goToScene(SCENE_PLAY)
+            elif self.quitButton.handleEvent(event):
+                self.quit()
+            elif self.highScoresButton.handleEvent(event):
+                self.goToScene(SCENE_HIGH_SCORES)
+
+    def draw(self):
+        # self.window.fill(BLACK)
+        self.startButton.draw()
+        self.quitButton.draw()
+        self.highScoresButton.draw()
